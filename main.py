c2609d17902e2659507760c8555e191cafd395b0
Varun Sharma <varun_sharma@live.co.uk>
2020-10-14 00:22:41 -0700

    flask 2nd push - templates still buggy

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
+from flask.templating import render_template_string
 # https://realpython.com/flask-by-example-part-1-project-setup/ for deploying with heroku later.
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+# @app.route("/")
+# def index():
+#     return render_template('index.html')
 
-app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template_string('Hello World')
 
 
 if __name__ == "__main__":
